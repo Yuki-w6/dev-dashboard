f5985173ec4aaa1f4a3cc4690eb604c416f62396
Yuki-w6 <m1610600@gmail.com>
2026-06-22 06:15:09 +0000

    設定値の変更

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// リポジトリ名に合わせて変更してください（例: '/my-dashboard/'）
 export default defineConfig({
   plugins: [react()],
-  base: '/REPO_NAME/',
-})
+  base: "/dev-dashboard/",
+});
